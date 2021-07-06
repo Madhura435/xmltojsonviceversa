45b6ebe2c297dcaa820d42ee1726b7b653b2b07e
Madhura453 <madhubhavi453@gmail.com>
2021-07-06 10:44:01 +0530

    xmltojsonViceversa

--- src/main/java/com/ja/madhura/xmltojson/Jsontoxml.java
+++ src/main/java/com/ja/madhura/xmltojson/Jsontoxml.java
@@ -1,5 +1,10 @@
 package com.ja.madhura.xmltojson;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -8,25 +13,15 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 public class Jsontoxml{
 	
-	 public static void main(String[] args) throws JsonMappingException, JsonProcessingException 
+	 public static void main(String[] args) throws IOException 
 	    {
-		 String jsonString = "" 
-			        + "{\n \"_links\": {\n" 
-			        + "    \"individual\": {\n" 
-			        + "      \"href\": \"/individuals/matching\",\n" 
-			        + "      \"name\": \"GET\",\n" 
-			        + "      \"title\": \"Individual Details\"\n" 
-			        + "    },\n" 
-			        + "    \"self\": {\n" 
-			        + "      \"href\": \"/individuals/matching/\"\n" 
-			        + "    }\n" //
-			        + "  }\n" //
-			        + "}";
-
-			ObjectMapper objectMapper = new ObjectMapper();
+		 
+		 String jsondata=FileUtils.readFileToString(new File("target/empjson.json"),"UTF-8");
+ObjectMapper objectMapper = new ObjectMapper();
 			ObjectMapper xmlMapper = new XmlMapper();
-			JsonNode tree = objectMapper.readTree(jsonString);
-			String jsonAsXml = xmlMapper.writer().withRootName("RootTagName").writeValueAsString(tree);
-	    System.out.println(tree);
+			JsonNode tree = objectMapper.readTree(jsondata);
+			String jsontoXml = xmlMapper.writer().withRootName("RootTagName").writeValueAsString(tree);
+			System.out.println("please find below json File to xml");
+			 System.out.println(jsontoXml);
 	    }
 }
